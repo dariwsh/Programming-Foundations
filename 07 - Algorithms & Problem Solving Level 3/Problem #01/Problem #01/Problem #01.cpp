c65b9e1774e88d51a543e248264fb6aa39e434a0
Ahmed Darwish <dariwshpro@gmail.com>
2026-07-08 22:56:46 +0300

    Add Couress 07 - Algorithms & Problem Solving Level 3

---
+++ 07 - Algorithms & Problem Solving Level 3/Problem #01/Problem #01/Problem #01.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int RandomNumber(int Form, int To)
+{
+	int RandNumber = rand() % (To - Form + 1) + Form;
+	return RandNumber;
+}
+
+void FillMatrixWithRandomNumbers(int arr[3][3], short Rows, short Cols)
+{
+	for (int i = 0; i < Rows; i++) 
+	{
+		for (int j = 0; j < Cols; j++)
+		{
+			arr[i][j] = RandomNumber(1, 100);
+		}
+	}
+}
+
+void PrintMatrix(int arr[3][3], short Rows, short Cols)
+{
+	for (int i = 0; i < Rows; i++)
+	{
+		for (int j = 0; j < Cols; j++)
+		{
+			cout << "   " << arr[i][j] << "  ";
+		}
+		cout << "\n";
+	}
+}
+
+int main()
+{
+	// بناءً على الوقت الحالي، عشان الأرقام تتغير كل مرة تشغل
+	srand((unsigned)time(NULL)); 
+	int arr[3][3];
+	FillMatrixWithRandomNumbers(arr, 3, 3);
+	PrintMatrix(arr, 3, 3);
+}
