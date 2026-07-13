4f71cb21deee7da0712934d43b2d4ce7bb10811e
Ahmed Darwish <dariwshpro@gmail.com>
2026-07-14 00:50:21 +0300

    Add lesson 13 to 15

--- 07 - Algorithms & Problem Solving Level 3/Problem #12/Problem #12/Problem #12.cpp
+++ 07 - Algorithms & Problem Solving Level 3/Problem #12/Problem #12/Problem #12.cpp
@@ -2,52 +2,66 @@
 #include <string>
 #include <iomanip>
 using namespace std;
-
+int RandomNumber(int From, int To)
+{
+	//Function to generate a random number
+	int randNum = rand() % (To - From + 1) + From;
+	return randNum;
+}
+void FillMatrixWithRandomNumbers(int arr[3][3], short Rows, short
+	Cols)
+{
+	for (short i = 0; i < Rows; i++)
+	{
+		for (short j = 0; j < Cols; j++)
+		{
+			arr[i][j] = RandomNumber(1, 10);
+		}
+	}
+}
 void PrintMatrix(int arr[3][3], short Rows, short Cols)
 {
 	for (short i = 0; i < Rows; i++)
 	{
 		for (short j = 0; j < Cols; j++)
 		{
-			cout << setw(3) << arr[i][j] << " ";
+			printf(" %0*d ", 2, arr[i][j]);
+			//cout << setw(3) << arr[i][j] << " ";
 		}
 		cout << "\n";
 	}
 }
 
-bool IsIdentityMarix(int Matrix1[3][3], short Rows, short Cols)
+bool AreTypicalMatrices(int Matrix1[3][3], int Matrix2[3][3],
+	short Rows, short Cols)
 {
-	for (int i = 0; i < Rows; i++)
+	for (short i = 0; i < Rows; i++)
 	{
-		for (int j = 0; j < Cols; j++)
+		for (short j = 0; j < Cols; j++)
 		{
-			if ( i == j && Matrix1[i][j] != 1)
-
-			{
-				return false;
-
-			}
-			else if (i != j && Matrix1[i][j] != 0)
+			if (Matrix1[i][j] != Matrix2[i][j])
 			{
 				return false;
 			}
-			
 		}
 	}
-
+	return true;
 }
-
-
-
 int main()
 {
-	int Matrix1[3][3]={{1,2,3},{4,5,6},{7,8,9}};
-	//int Matrix1[3][3] = { {1,0,0},{0,1,0},{0,0,1} };
+	//Seeds the random number generator in C++, called only once
+	srand((unsigned)time(NULL));
+	int Matrix1[3][3], Matrix2[3][3];
+	FillMatrixWithRandomNumbers(Matrix1, 3, 3);
 	cout << "\nMatrix1:\n";
 	PrintMatrix(Matrix1, 3, 3);
-	if (IsIdentityMarix(Matrix1, 3, 3))
-		cout << "\nYES: Matrix is identity.";
+	FillMatrixWithRandomNumbers(Matrix2, 3, 3);
+	cout << "\nMatrix2:\n";
+	PrintMatrix(Matrix2, 3, 3);
+	if (AreTypicalMatrices(Matrix1, Matrix2, 3, 3))
+		cout << "\nYES: both martices are typical.";
 	else
-		cout << "\nNo: Matrix is NOT identity.";
+		cout << "\nNo: martices are NOT typical.";
+	system("pause>0");
 
 }
---
+++ 07 - Algorithms & Problem Solving Level 3/Problem #14/Problem #14/Problem #14.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+void PrintMatrix(int arr[3][3], short Rows, short Cols)
+{
+	for (short i = 0; i < Rows; i++)
+	{
+		for (short j = 0; j < Cols; j++)
+		{
+			cout << setw(3) << arr[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
+
+bool IsScalarMarix(int Matrix1[3][3],
+	short Rows, short Cols)
+{
+	int firstElem = Matrix1[0][0];
+	for (short i = 0; i < Rows; i++)  
+	{
+		for (short j = 0; j < Cols; j++)
+		{
+			if (i == j && Matrix1[i][j] != firstElem )
+			{
+				return false;
+			}
+			else if (i != j && Matrix1[i][j] != 0)
+			{
+				return false;
+			}
+		}
+	}
+	return true;
+}
+int main()
+{
+
+	int Matrix1[3][3] = { {9,0,0},{0,9,0},{0,0,9} };
+	cout << "\nMatrix1:\n";
+	PrintMatrix(Matrix1, 3, 3);
+	if (IsScalarMarix(Matrix1, 3, 3))
+		cout << "\nYES: Matrix is scalar.";
+	else
+		cout << "\nNo: Matrix is NOT scalar.";
+}
